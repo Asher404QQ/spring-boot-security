781674667deebd7f820f87123ada672d11763c4e
Korsakov <kors2222@yandex.ru>
2024-05-22 21:42:33 +0300

    Init commit

---
+++ src/main/java/ru/kors/config/ProjectConfig.java
@@ -0,0 +1,38 @@
+package ru.kors.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import ru.kors.security.services.SimpleUser;
+
+@Configuration
+public class ProjectConfig{
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(
+                        auth -> auth.anyRequest().authenticated()
+                ).formLogin(Customizer.withDefaults()).build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        var userDetailsService = new InMemoryUserDetailsManager();
+        var user1 = new SimpleUser("anton", passwordEncoder.encode("12345"));
+        userDetailsService.createUser(user1);
+        return userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
+
