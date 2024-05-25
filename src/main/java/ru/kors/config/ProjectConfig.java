cea745da5e19d05c3dc5d18121b6f7e03c6782ee
Korsakov <kors2222@yandex.ru>
2024-05-25 23:52:11 +0300

    fake

--- src/main/java/ru/kors/config/ProjectConfig.java
+++ src/main/java/ru/kors/config/ProjectConfig.java
@@ -7,15 +7,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import ru.kors.models.MyUser;
 import ru.kors.security.services.InMemoryUserDetailsService;
 import ru.kors.security.services.SecurityUser;
 import ru.kors.security.services.Sha512PasswordEncoder;
 
-import javax.sql.DataSource;
 import java.util.List;
 
 @Configuration
@@ -41,7 +40,9 @@ public class ProjectConfig{
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new Sha512PasswordEncoder();
+        PasswordEncoder spe = new StandardPasswordEncoder("secret");
+        PasswordEncoder pbkdf2 = new Pbkdf2PasswordEncoder("secret", 185000, 1, 256);
+        return new BCryptPasswordEncoder(1);
     }
 }
 
