c2f76fdc1bfd3ac1349d7b493e6f4c2bb6659ee4
Korsakov <kors2222@yandex.ru>
2024-05-28 22:11:25 +0300

    SecurityContext

--- src/main/java/ru/kors/config/ProjectConfig.java
+++ src/main/java/ru/kors/config/ProjectConfig.java
@@ -1,9 +1,12 @@
 package ru.kors.config;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,7 +16,15 @@ import org.springframework.security.web.SecurityFilterChain;
 import javax.sql.DataSource;
 
 @Configuration
+@EnableAsync
 public class ProjectConfig {
+
+//    @Bean
+//    public InitializingBean initializingBean() {
+//        return () -> SecurityContextHolder.setStrategyName(
+//                SecurityContextHolder.MODE_INHERITABLETHREADLOCAL
+//        );
+//    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
--- src/main/java/ru/kors/controller/HelloController.java
+++ src/main/java/ru/kors/controller/HelloController.java
@@ -1,13 +1,49 @@
 package ru.kors.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
+import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+
 @RestController
 public class HelloController {
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/bye")
+    public String bye() throws Exception {
+        Callable<String> task = () -> {
+            var context = SecurityContextHolder.getContext();
+            var auth = context.getAuthentication();
+            return auth.getName();
+        };
+
+        var exec = new DelegatingSecurityContextExecutorService(Executors.newCachedThreadPool());
+
+        try {
+            return "Bye, " + exec.submit(task).get() + "!";
+        } finally {
+            exec.shutdown();
+        }
+    }
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello";
+    public String hello(Authentication auth) {
+        return "Hello, " + auth.getName() + "!";
+    }
+
+    @GetMapping("/async")
+    @Async
+    public void async() {
+        var context = SecurityContextHolder.getContext();
+        var auth = context.getAuthentication();
+        logger.info("Principal name={}, authorities={}", auth.getName(), auth.getAuthorities());
     }
 }
